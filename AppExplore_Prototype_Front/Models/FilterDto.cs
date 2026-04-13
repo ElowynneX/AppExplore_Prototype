245a2e28aa0306188f142459efba622ad1bb0568
Elowynne Xiong <chackyxyooj@hotmail.com>
2026-04-13 01:10:15 -0400

    Front End Added
    Yup. That's It.

---
+++ AppExplore_Prototype_Front/Models/FilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AppExplore_API_Prototype.Model
+{
+    public class FilterDto
+    {
+        public string SearchCategory { get; set; }
+        public List<string> SearchType { get; set; }
+        public List<string> SearchTags { get; set; }
+    }
+}
---
+++ AppExplore_Prototype_Front/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppExplore_Prototype_Front.Pages.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
